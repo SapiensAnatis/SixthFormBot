3dd235753e1de30e35a9420fbaee86088da178d3
Jay Malhotra <jay0@me.com>
2017-09-23 19:58:00 +0100

    Convert to use cogs and discord.commands

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 """main.py is the executing file which starts the bot & then loads the cogs."""
 import discord
-from config import *
+from discord.ext import commands
+# Import config dict
+from config import config
 # Import utils logging functions
 from utils import log
 
-client = discord.Client()
+initial_extensions = ["cogs.subject_reader"]
+bot = commands.Bot(command_prefix="~", description="Sixth form bot")
 
 log("Connecting to Discord...", "init")
 
-@client.event
+@bot.event
 async def on_ready():
     """Function call for when the bot is initialized."""
 
-    log(f'Successfully logged in as {client.user}.', "on_ready()")
+    log(f"Successfully logged in as {bot.user}.", "on_ready()")
+    log(f"Loading modules...", "on_ready()")
+    # Load cogs
+    if __name__ == "__main__":
+        for extension in initial_extensions:
+            try:
+                print(f"\t{extension[5:]}...", end="")
+                bot.load_extension(extension)
+                print("done!")
+            except ImportError:
+                log(f"Could not find cog {extension}. Check that it exists.", "on_ready()", "error")
+            except discord.ClientException:
+                log(f"Cog {extension} was invalid (no setup func).", "on_ready()", "error")
 
+    log(f"All modules successfully loaded.", "on_ready()")
 
-client.run(f'{config["discord_token"]}')
+bot.run(f'{config["discord_token"]}', bot=True, reconnect=True)
