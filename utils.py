c70e5c578f194778957f8da1f845fdf2d584c84d
Jay Malhotra <jay0@me.com>
2017-09-23 17:59:49 +0100

    Switch to config.py style of configuration rather than raw text files
    It's easier this way, as a result I've eliminated utils' localize filepath function - as getting tokens/keys is all I could see it being used for.

--- utils.py
+++ utils.py
@@ -1,11 +1,24 @@
+"""
+The intention of utils.py is to provide handy functions without having any dependencies
+within the project which does not depend on the project in any way. Functions in here are
+generally used in order to save long lines of code from being used to perform tasks which
+are repeated often.
+"""
+
 import datetime
 import os
 
 __maindirectory__ = os.path.dirname(__file__)
 
-"""Used for outputting messages with timestamps and severity/source descriptors. Should always be used over lone print statements."""
 def log(message, source, severity="info"):
-    print(f"[{datetime.datetime.now():%H:%M:%S}][{severity}] {source}: {message}")
+    """
+    Used for outputting messages with timestamps and severity/source descriptors.
+    Should always be used over lone print statements.
 
-def localize_filepath(filepath):
-    return os.path.join(__maindirectory__, filepath)
+    Keyword args:
+    message -- the message to log to the console
+    source -- a description of where the log function is being invoked
+    severity -- a string describing the severity, e.g. info, debug, error, warning (default info)
+    """
+
+    print(f"[{datetime.datetime.now():%H:%M:%S}][{severity}] {source}: {message}")
