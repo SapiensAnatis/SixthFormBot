0018c7b7205d790b9c75bed94b6fe8fcb6b2c2e3
Jay Malhotra <jay0@me.com>
2017-09-23 18:00:36 +0100

    Convert main.py to new system

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
+"""main.py is the executing file which starts the bot & then loads the cogs."""
 import discord
-# Import utils logging functions, for logging, and path functions to retrieve bot token
-from utils import log, localize_filepath
+from config import *
+# Import utils logging functions
+from utils import log
 
 client = discord.Client()
 
 log("Connecting to Discord...", "init")
 
-# Called when the bot is fully initialized.
 @client.event
 async def on_ready():
+    """Function call for when the bot is initialized."""
+
     log(f'Successfully logged in as {client.user}.', "on_ready()")
 
-# Function to get token on startup. Would rather not pollute global scope with these variables
-def get_token():
-    # Retrieve token to log in with
-    token_filepath = localize_filepath("keychain/discord_token.txt")
-    token_file = open(token_filepath)
-    return token_file.read()
 
-client.run(f'{get_token()}')
+client.run(f'{config["discord_token"]}')
